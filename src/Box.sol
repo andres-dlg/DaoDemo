115b4c6560caa217c24a50d8414da0e5e605f4ed
andres-dlg <andresdlg21@gmail.com>
2024-12-28 00:15:47 -0300

    Initial commit

---
+++ src/Box.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    constructor(address ownerAddress) Ownable(ownerAddress) {}
+
+    uint256 private value;
+
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 newValue);
+
+    // Stores a new value in the contract
+    function store(uint256 newValue) public onlyOwner {
+        value = newValue;
+        emit ValueChanged(newValue);
+    }
+
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return value;
+    }
+}
